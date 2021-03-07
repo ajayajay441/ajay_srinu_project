48de3da8558c6279ce6b0b8335d21dbbcaf8dd4b
Srinivas Nagalla <srinivasnagalla@Srinivass-MacBook-Air.local>
2021-03-07 17:16:38 +0530

    styles.scss

--- src/app/shipmentspage/shipmentspage.component.ts
+++ src/app/shipmentspage/shipmentspage.component.ts
@@ -1,40 +1,36 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { HttpClient } from '@angular/common/http';
-import { ShipmentService } from '../_services/shipments.service';
-import { Subscription } from 'rxjs/index';
-import { switchMap } from 'rxjs/operators';
-import { AuthenticationService } from '../_services';
-import { DashboardService } from '../_services/dashboard.service';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { HttpClient } from "@angular/common/http";
+import { ShipmentService } from "../_services/shipments.service";
+import { Subscription } from "rxjs/index";
+import { switchMap } from "rxjs/operators";
+import { AuthenticationService } from "../_services";
+import { DashboardService } from "../_services/dashboard.service";
 
 @Component({
-  selector: 'app-shipmentspage',
-  templateUrl: './shipmentspage.component.html',
-  styleUrls: ['./shipmentspage.component.scss']
+  selector: "app-shipmentspage",
+  templateUrl: "./shipmentspage.component.html",
+  styleUrls: ["./shipmentspage.component.scss"],
 })
 export class ShipmentspageComponent implements OnInit {
-  data:any = [];
-  shipments = [
-    { name: 'cargo', seemore: false },
-    { name: 'Booked', seemore: false },
-  ];
-  viewShipmentsType = 'bookmarked';
+  data: any = [];
+  shipments = [];
+  viewShipmentsType = "bookmarked";
 
   constructor(
     private router: Router,
     private http: HttpClient,
     private shipmentService: ShipmentService,
     private authenticationService: AuthenticationService,
-    private dashboardService: DashboardService,
-
-  ) { }
+    private dashboardService: DashboardService
+  ) {}
 
   ngOnInit(): void {
-    this.getDashboardShipments()
+    this.getDashboardShipments();
   }
 
   toggleShipmentDataView(shipment: any) {
-    console.log(shipment, 'card expand or collapse');
+    console.log(shipment, "card expand or collapse");
   }
   // getShipmentDetails(value?:string) {
   //   let status='';
@@ -59,13 +55,13 @@ export class ShipmentspageComponent implements OnInit {
   //   });
   // }
   getDashboardShipments(value?: string) {
-    let status = '';
-    if (value == 'Bookmarked') {
-      status = 'BOOKMARK';
-    } else if (value == 'Arriving') {
-      status = 'ARRIVING';
-    } else if (value == 'Booked') {
-      status = 'BOOKED';
+    let status = "";
+    if (value == "Bookmarked") {
+      status = "BOOKMARK";
+    } else if (value == "Arriving") {
+      status = "ARRIVING";
+    } else if (value == "Booked") {
+      status = "BOOKED";
     }
     this.authenticationService
       .refreshToken()
@@ -80,10 +76,10 @@ export class ShipmentspageComponent implements OnInit {
       )
       .subscribe((response: any) => {
         this.data = response.Shipments;
-        this.data.forEach(function (element:any) {
-                element.seemore = false;
-              });
-        console.log('ship resp', response.Shipments);
+        // this.data.forEach(function (element:any) {
+        //         element.seemore = false;
+        //       });
+        console.log("ship resp", response.Shipments);
       });
   }
 }
