f0cdb75bab0e038b6c8201e5ebf0ea62c2d6a1ea
Srinivas Nagalla <srinivasnagalla@Srinivass-MacBook-Air.local>
2021-03-15 00:15:40 +0530

    original design of Contract tab

--- src/app/quotationspage/contractslist/contractslist.component.ts
+++ src/app/quotationspage/contractslist/contractslist.component.ts
@@ -1,94 +1,98 @@
-import { Component, Input, OnInit, ViewChild } from '@angular/core';
+import { Component, Input, OnInit, ViewChild } from "@angular/core";
 import { MatSort } from "@angular/material/sort";
-import { animate, state, style, transition, trigger } from '@angular/animations';
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger,
+} from "@angular/animations";
 import { MatTableDataSource } from "@angular/material/table";
 
 @Component({
-  selector: 'app-contractslist',
-  templateUrl: './contractslist.component.html',
-  styleUrls: ['./contractslist.component.scss'],
+  selector: "app-contractslist",
+  templateUrl: "./contractslist.component.html",
+  styleUrls: ["./contractslist.component.scss"],
   animations: [
-    trigger('detailExpand', [
-      state('collapsed', style({ height: '0px', minHeight: '0' })),
-      state('expanded', style({ height: '*' })),
-      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    trigger("detailExpand", [
+      state("collapsed", style({ height: "0px", minHeight: "0" })),
+      state("expanded", style({ height: "*" })),
+      transition(
+        "expanded <=> collapsed",
+        animate("225ms cubic-bezier(0.4, 0.0, 0.2, 1)")
+      ),
     ]),
   ],
 })
 export class ContractslistComponent implements OnInit {
   @Input() filter: string;
   displayedColumns: string[] = [
-    "qtno",
-    "reference",
-    "origin",
-    "destination",
-    "qtexpiry",
-    "amount",
-    "weight",
-    "service",
-    "status",
+    "contractRef",
+    "validFrom",
+    "validTo",
+    "signedby",
     "details",
+  ];
+  displayedColumns1: string[] = [
+    "service",
+    "from",
+    "to",
+    "term",
+    "createBooking",
+  ];
+  ELEMENT_DATA1: SubElement[] = [
+    {
+      service: "FSL-98001",
+      from: "Jan 1, 2021",
+      to: "Jan 1, 2021",
+      term: "Hemand",
+      createBooking: "Details",
+    },
   ];
   ELEMENT_DATA: PeriodicElement[] = [
     {
-      qtno: "US-ABC",
-      reference: "US-GHI",
-      origin: "America",
-      destination: "Lyon",
-      qtexpiry: "20/01/2021",
-      amount: "AED 2000",
-      weight: "2334 kg.",
-      service: "FCL IMP",
-      status: "Pending",
-      details: "Details",
-    },
-    {
-      qtno: "US-DEF",
-      reference: "US-JKL",
-      origin: "Brazil",
-      destination: "Xyon",
-      qtexpiry: "20/01/2021",
-      amount: "AED 2000",
-      weight: "2334 kg.",
-      service: "FCL IMP",
-      status: "Pending",
+      contractRef: "FSL-98001",
+      validFrom: "Jan 1, 2021",
+      validTo: "Jan 1, 2021",
+      signedby: "Hemand",
       details: "Details",
     },
   ];
   dataSource = new MatTableDataSource(this.ELEMENT_DATA);
+  dataSource1 = new MatTableDataSource(this.ELEMENT_DATA1);
   expandedElement: PeriodicElement | null = null;
 
   @ViewChild(MatSort)
   sort!: MatSort;
 
   constructor() {
-    this.filter = '';
+    this.filter = "";
   }
 
   ngOnChanges(changes: any) {
     const filterValue = changes.filter.currentValue;
-    this.dataSource.filter = filterValue.trim().toLowerCase()
+    this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 
   ngAfterViewInit() {
     this.dataSource.sort = this.sort;
   }
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
 
 export interface PeriodicElement {
-  qtno: string;
-  reference: string;
-  origin: string;
-  destination: string;
-  qtexpiry: string;
-  amount: string;
-  weight: string;
-  service: string;
-  status: string;
+  contractRef: string;
+  validFrom: string;
+  validTo: string;
+  signedby: string;
   details: string;
-  isExpanded?: boolean
+  isExpanded?: boolean;
+}
+export interface SubElement {
+  service: string;
+  from: string;
+  to: string;
+  term: string;
+  createBooking: string;
 }
