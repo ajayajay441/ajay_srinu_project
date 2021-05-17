fbc257082a0e7c95df5c83855c93020cf69256e7
Srinivas Nagalla <srinivasnagalla@Srinivass-MacBook-Air.local>
2021-05-17 13:29:48 +0530

    changes as per adrien mail

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { MatIconRegistry } from "@angular/material/icon";
 import { DomSanitizer } from "@angular/platform-browser";
+import { Router } from "@angular/router";
 @Component({
   selector: "app-header",
   templateUrl: "./header.component.html",
@@ -9,8 +10,13 @@ import { DomSanitizer } from "@angular/platform-browser";
 export class HeaderComponent implements OnInit {
   constructor(
     private domSanitizer: DomSanitizer,
-    private matIconRegistry: MatIconRegistry
+    private matIconRegistry: MatIconRegistry,
+    private router: Router
   ) {}
 
   ngOnInit(): void {}
+  goTo(routePageName: string) {
+    // console.log("data", data);
+    this.router.navigate([`${routePageName}`]); // navigate to other page
+  }
 }
